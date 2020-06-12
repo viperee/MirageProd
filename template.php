3ef17a733223dea3c1fd61a068b11aeea0a7ca40
viperee <just.b.nd.l@gmail.com>
2020-06-12 18:50:19 +0200

    Ajout fichier php squelette

---
+++ template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <title>TITRE</title>
+    <?php include("head.php"); ?>
+</head>
+<body>
+<?php include("header.php"); ?>
+
+<main class="wrapper">
+</main>
+
+<?php include("footer.php"); ?>
+
+<!--IMPORTS JS-->
+<?php include("script.php"); ?>
+<!--/IMPORTS JS-->
+
+</body>
+</html>
