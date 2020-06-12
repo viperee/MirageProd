517ae6c6ea527c0a256167865675b3f048717342
viperee <just.b.nd.l@gmail.com>
2020-06-12 21:48:39 +0200

    Integration icones + update icone page d'accueil

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <a href="index.php"><img class="logo" src="assets/media/img/logo.png" alt="Logo"></a>
         </div>
         <div class="col-1 offset-9">
-            <img id="open-sidenav-btn" src="assets/media/img/Menu.svg" alt="Menu">
+            <img id="open-sidenav-btn" src="assets/icons/menu.svg" alt="Menu">
         </div>
     </div>
     <?php include("sidenav.php"); ?>
