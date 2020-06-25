5703f9157a98a617ec71cc74a18c542cfa231df1
viperee <just.b.nd.l@gmail.com>
2020-06-25 11:24:16 +0200

    Page infos pratiques responsive OK

--- infos-pratiques.php
+++ infos-pratiques.php
@@ -49,7 +49,7 @@
             </div>
         </div>
         <div id="tabs-container" class="background-solitude">
-            <div id="tabs-trajet">
+            <div id="tabs-trajet" class="container-fluid">
                 <div class="block-accordeon mb-4 container background-white">
                     <div class="row">
                         <div class="col-6">
@@ -60,15 +60,17 @@
                         </div>
                     </div>
                     <div class="content-accordeon">
-                        <p class="texte-accordeon">Vous avez prévu d’aller au Mirage Festival et vous avez des places
-                            libres
-                            dans votre voiture? Ou vous avez déjà acheté votre billet pour le
-                            festival, mais vous ne savez pas comment vous y rendre ?
+                    <p class="sous-titre-accordeon">A l'aller</p>
+                        <p class="texte-accordeon">Afin de garantir la meilleure fluidité de trafic, merci de vous 
+                        conformer aux indications et à la signalisation mise en place.
+                        
+                        Bordeaux est facilement accessible aux automobilistes, notamment grâce au noeud 
+                        d’interconnexion autoroutier A10 / A 62 / A63/ A89/A65.
                         </p>
-                        <p class="sous-titre-accordeon">La solution</p>
-                        <p class="texte-accordeon">Covoiturez avec notre partenaire IDVroom, vous pourrez ainsi
-                            rencontrer d’autres festivaliers et partager votre passion pour la
-                            musique.
+                        <p class="sous-titre-accordeon">Parkings</p>
+                        <p class="texte-accordeon">Des parkings sont mis à votre disposition à proximité du site 
+                        du Festival. Ces parkings peuvent être fermés en cas d'intempéries. 
+                        Des parkings de remplacement sont dès lors proposés en ville et dans ses alentours.<br><br><br>
                         </p>
                     </div>
                 </div>
@@ -90,11 +92,11 @@
                         <p class="sous-titre-accordeon">La solution</p>
                         <p class="texte-accordeon">Covoiturez avec notre partenaire IDVroom, vous pourrez ainsi
                             rencontrer d’autres festivaliers et partager votre passion pour la
-                            musique.
+                            musique.<br><br>
                         </p>
                         <div class="text-center pb-5">
                             <a href="https://www.idvroom.com/" target="_blank"><button id="btn-covoiturer"><span>Covoiturer</span></button></a>
-                        </div>
+                        </div><br>
                     </div>
                 </div>
                 <div class="block-accordeon mb-4 container background-white">
@@ -107,15 +109,10 @@
                         </div>
                     </div>
                     <div class="content-accordeon">
-                        <p class="texte-accordeon">Vous avez prévu d’aller au Mirage Festival et vous avez des places
-                            libres
-                            dans votre voiture? Ou vous avez déjà acheté votre billet pour le
-                            festival, mais vous ne savez pas comment vous y rendre ?
-                        </p>
-                        <p class="sous-titre-accordeon">La solution</p>
-                        <p class="texte-accordeon">Covoiturez avec notre partenaire IDVroom, vous pourrez ainsi
-                            rencontrer d’autres festivaliers et partager votre passion pour la
-                            musique.
+                        <p class="texte-accordeon">Rendez-vous au festival en bus grâce aux sites suivants :<br><br>
+                            <a class="liens-habitants" href="https://www.flixbus.fr/" target="_blank">Flixbus</a><br>
+                            <a class="liens-habitants" href="https://www.ouibus.com" target="_blank">Ouibus</a><br>
+                            <a class="liens-habitants" href="https://www.eurolines.com" target="_blank">Eurolines</a><br><br>
                         </p>
                     </div>
                 </div>
@@ -129,15 +126,9 @@
                         </div>
                     </div>
                     <div class="content-accordeon">
-                        <p class="texte-accordeon">Vous avez prévu d’aller au Mirage Festival et vous avez des places
-                            libres
-                            dans votre voiture? Ou vous avez déjà acheté votre billet pour le
-                            festival, mais vous ne savez pas comment vous y rendre ?
-                        </p>
-                        <p class="sous-titre-accordeon">La solution</p>
-                        <p class="texte-accordeon">Covoiturez avec notre partenaire IDVroom, vous pourrez ainsi
-                            rencontrer d’autres festivaliers et partager votre passion pour la
-                            musique.
+                    <p class="texte-accordeon">Rendez-vous au festival en train grâce aux sites suivants :<br><br>
+                            <a class="liens-habitants" href="https://www.ouigo.com/" target="_blank">Ouigo</a><br>
+                            <a class="liens-habitants" href="https://www.trainline.fr" target="_blank">Trainline</a><br><br>
                         </p>
                     </div>
                 </div>
@@ -151,16 +142,11 @@
                         </div>
                     </div>
                     <div class="content-accordeon">
-                        <p class="texte-accordeon">Vous avez prévu d’aller au Mirage Festival et vous avez des places
-                            libres
-                            dans votre voiture? Ou vous avez déjà acheté votre billet pour le
-                            festival, mais vous ne savez pas comment vous y rendre ?
+                        <p class="texte-accordeon">Le Mirage Festival investi Bordeaux, pour vous y retrouver dans la ville, aidez-vous du plan en ligne:<br /><br />
                         </p>
-                        <p class="sous-titre-accordeon">La solution</p>
-                        <p class="texte-accordeon">Covoiturez avec notre partenaire IDVroom, vous pourrez ainsi
-                            rencontrer d’autres festivaliers et partager votre passion pour la
-                            musique.
-                        </p>
+                        <div class="text-center pb-5">
+                            <a href="https://www.infotbm.com/fr/plans/plan-dynamique/lines/" target="_blank"><button id="btn-covoiturer"><span>Plan en ligne</span></button></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -173,6 +159,19 @@
                         <div class="offset-5 margin-vertical-center">
                             <img class="titre-accordeon-icon" src="assets/icons/arrow-down.svg">
                         </div>
+                    </div>
+                    <div class="content-accordeon">
+                        <p class="sous-titre-accordeon">Bars</p>
+                        <p class="texte-accordeon">D'une programmation à l'autre, les bars se succèdent 
+                        pour étancher toutes les soifs. Envie d’une bière spéciale? C’est du côté du quartier saint-michel
+                        que ça se passe. D’une boisson rafraîchissante sans alcool? Le prochain bar sera le bon! 
+                        D’une sélection de cépages? Les bars à vins sont là!<br><br>
+                        </p>
+                        <p class="sous-titre-accordeon">Restaurants</p>
+                        <p class="texte-accordeon">D'une programmation à l'autre', les restaurants se succèdent 
+                        pour restaurer tous les festivaliers. Envie d’une pizza? C’est du côté du quartier saint-michel
+                        que ça se passe. D’un mets local? Le prochain bar sera le bon!<br><br><br>
+                        </p>
                     </div>
                 </div>
                 <div class="block-accordeon mb-4 container background-white">
@@ -192,7 +191,7 @@
                         <p class="texte-accordeon">Les personnes accèdent à l’intérieur de l’enceinte du Festival par l’entrée principale (couloir de droite ABONNEMENT/HANDICAP).</p>
                         <p class="sous-titre-accordeon">Service de transport à tarif préférentiel</p>
                         <p class="texte-accordeon">Transport Handicap s’engage pour l’accès à la culture des personnes handicapées. A cet effet, il propose un service de transport avec des véhicules adaptés aux personnes à mobilité réduite, à tarif préférentiel (entre 20 et 40 euros, aller et retour, selon la distance).<br><br>
-                            Cette offre est limitée au canton à Bordeaux et uniquement sur réservation, au moins 24h à l’avance. Elle ne comprend pas le billet d’entrée au festival, qui doit être acheté séparément.</p>
+                            Cette offre est limitée à Bordeaux et uniquement sur réservation, au moins 24h à l’avance. Elle ne comprend pas le billet d’entrée au festival, qui doit être acheté séparément.<br><br></p>
                     </div>
                 </div>
                 <div class="block-accordeon mb-4 container background-white">
@@ -223,6 +222,15 @@
                             <img class="titre-accordeon-icon" src="assets/icons/arrow-down.svg">
                         </div>
                     </div>
+                    <div class="content-accordeon pb-5">
+                        <p class="texte-accordeon">Pour vous renseigner sur les possibilités de logement de la région, 
+                        consultez les sites suivants :<br><br>
+                        <a class="liens-habitants" href="https://www.campingfrance.com/" target="_blank">Camping France</a><br>
+                        <a class="liens-habitants" href="https://www.yellohvillage.fr/" target="_blank">Yelloh Village</a><br>
+                        <a class="liens-habitants" href="https://www.maeva.com/fr-fr/" target="_blank">Maeva</a><br>
+                        <a class="liens-habitants" href="https://www.homair.com/" target="_blank">Homair</a>
+                        </p>
+                    </div>
                 </div>
                 <div class="block-accordeon mb-4 container background-white">
                     <div class="row">
@@ -234,7 +242,11 @@
                         </div>
                     </div>
                     <div class="content-accordeon pb-5">
-                        <p class="texte-accordeon">Pour vous renseigner sur les possibilités de logement de la région, consultez les sites suivants :<br><br><a class="liens-habitants" href="https://www.airbnb.fr/" target="_blank">Airbnb</a><br><a class="liens-habitants" href="https://www.booking.com" target="_blank">Booking</a></p>
+                        <p class="texte-accordeon">Pour vous renseigner sur les possibilités de logement de la région, 
+                        consultez les sites suivants :<br><br>
+                        <a class="liens-habitants" href="https://www.airbnb.fr/" target="_blank">Airbnb</a><br>
+                        <a class="liens-habitants" href="https://www.booking.com" target="_blank">Booking</a>
+                        </p>
                     </div>
                 </div>
             </div>
