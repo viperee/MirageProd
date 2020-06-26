f6ef441ae617a0d00c6743fb9bdddccb141e30eb
viperee <just.b.nd.l@gmail.com>
2020-06-26 13:59:13 +0200

    Contact

--- contact.php
+++ contact.php
@@ -19,22 +19,24 @@
     </div>
     
     
-    <div class="no-padding container-fluid ">
-        <div class="background-solitude festival-description-wrapper">
-            <div id="festival-details" class="background-white">
+    <div class="no-padding container-fluid background-solitude">
+        <div class="background-solitude formulaire-wrapper">
+            <div class="background-white">
                 <div class="row">
                     <div class="col">
-                        <div class="festival-details-text">
-                            <h5 class="no-margin-top color-noir">Formulaire</h5>
+                        <div class="formulaire-details">
+                            <h5 class="no-margin-top color-noir">Contact</h5>
                             <form>
                                 <input id="nom" class="col-5" type="text" placeholder="Nom">
                                 <input id="mail" class="col-5" type="text" placeholder="Adresse Email">
                                 <input id="sujet" class="col-5" type="text" placeholder="Sujet">
-                                <textarea id="message" class="col-5" type="text" placeholder="Message"></textarea>
-                                <div class="col-12 text-center"><button id="btn-formulaire" class="btn-with-icon-black"><span>Envoyer</span></button></div>
+                                <textarea id="message" class="col-5" type="text" placeholder="Message" rows="8"></textarea>
                             </form>
                         </div>
                     </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center btn-formulaire-wrapper"><button id="btn-formulaire" class="btn-with-icon-black"><span>Envoyer</span></button></div>
                 </div>
             </div>
             <br/><br/><br/><br/>
